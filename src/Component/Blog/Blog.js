8f6510e6f01f4b3279c91a6c6c7f6da6ce7b4915
Debakor10 <debakor10@gmail.com>
2021-11-04 21:59:57 +0600

    updated

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -4,19 +4,19 @@ import './Blog.css'
 const article = [
     {
         img: "https://golapganjultrasono.debakor.com/wp-content/uploads/2020/11/articlk.jpg",
-        name: "Family Health Care Project",
-        descrip: "Golapganj Ultrasono Center is an organization of all good health care activities for the people from social responsibility."
+        name: "পারিবারিক স্বাস্থ্য পরিচর্যা প্রকল্প",
+        descrip: "সামাজিক দ্বায়বদ্ধতা থেকে জনগণের জন্য স্বাস্থ্যসেবার সকল শুভ কর্মকাণ্ডের প্রতিষ্টান গোলাপগঞ্জ আল্ট্রাসনো সেন্টার।"
 
     },
     {
         img: "https://golapganjultrasono.debakor.com/wp-content/uploads/2020/11/golapgonj-altasno-center-300x198-1.jpg",
-        name: "Official inauguration of Golapganj Ultrasono Center",
-        descrip: "was present as the Chief Guest at the inauguration ceremony of Golapganj Ultrasnow Center established on the initiative of Dr. Titu Chandra Acharya"
+        name: "গোলাপগঞ্জ আল্ট্রাসনো সেন্টারের আনুষ্ঠানিক উদ্বোধন",
+        descrip: "ডাঃ টিটু চন্দ্র আচার্য্যের উদ্যেগে প্রতিষ্ঠত গোলাপগঞ্জ আলট্রাস্নো সেন্টারের উদ্ধোধনী অনুষ্ঠানে প্রধান অতিথি হিসেবে উপস্থিত ছিলেন গোলাপগঞ্জ পৌরসভার নব-নির্বাচিত মেয়র আমিনুল ইসলাম রাবেল।"
     },
     {
         img: "https://golapganjultrasono.debakor.com/wp-content/uploads/2020/11/a.jpg",
-        name: "Family Health Card for poor people",
-        descrip: "In order to improve the quality of medical services for the people of Golapganj, a 'Golapganj Ultrasono Center' has been established on Zakiganj Road in Chaumuhani under the overall management of Dr. Titu Chandra Acharya"
+        name: "দরিদ্র মানুষের জন্য পারিবারিক স্বাস্থ্য কার্ড",
+        descrip: "গোলাপগন্জবাসীর চিকিৎসা সেবার মানোন্নয়নের লক্ষ্যে চৌমুহনীর জকিগঞ্জ রোডে প্রতিষ্ঠিত হয়েছে ‘গোলাপগন্জ আলট্রাসনো সেন্টার‘- যার সার্বিক পরিচালনায় আছেন অত্র এলাকার কৃতি ছাত্র ডা:টিটু চন্দ্র আচার্য্য।সামাজিক উন্নয়ন ও সেবা মূলক কাজ করার  লক্ষ্য ও অরাজনৈতিক সেচ্ছাসেবী মনোভাবের দৃষ্টিকোন নিয়ে  মানবতার সেবায় এগিয়ে এসেছে গোলাপগঞ্জ আলট্রাসোনো সেন্টার।"
 
     }
 ]
